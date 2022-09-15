71fae536090e7c84b7bfbc84cc96ee9eab16d581
lwazilwando <lwazimath10@gmail.com>
2022-09-15 11:31:02 +0200

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,78 @@
+import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Image, ImageBackground } from 'react-native';
+import { View } from 'react-native-web';
+
+
 
 export default function App() {
+
+
+  const [titleText, setTitleText] = useState("HAPPY BIRTHDAY LWANDO MASINGA")
+  const [bodyText, setBodyText] = useState("Wishing you all the best young man in this journey of life grow up an take care of momie")
+  const [lastText, setLastText] = useState("shobane kamangethe zikalala hobe mbokwane masinga \n omhlephe  ogibela igagasi sgengeh")
+  
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+    <ImageBackground source={require('./assets/cake_1550.png')} resizeMode='center' style={styles.image}>
+        <Text style={styles.titleText}>{titleText}{"\n"}</Text>
+       
+        <Image
+          style={styles.stretch}
+          source={require('./assets/lwando.png')}
+        />
+        
+        <Text style={styles.bodyText}>{bodyText}{"\n"}</Text>
+        <Text style={styles.lastText}>{lastText}{"\n"}</Text>
+
+        
+
+    </ImageBackground>
+
+    
   );
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  image:{
+    flex:1,
+    justifyContent:"center",
+    alignItems:"center",
+    justifyContent:"center",
   },
+  titleText:{
+    fontSize:"20px",
+    fontWeight:"bold",
+    color:"blue",
+
+  },
+  bodyText:{
+
+    fontSize:'30px',
+    fontWeight:"bold",
+    color:"red",
+  
+
+  },
+  lastText:{
+    
+    marginTop:'25px',
+    fontWeight:"bold",
+    color:"blue",
+    fontSize:'30px',
+  },
+  stretch:{
+    marginTop:"20px",
+    width:400,
+    height:300,
+    resizeMode:'stretch',
+    justifyContent:'flex-start',
+    alignSelf:'center',
+    paddingBottom:'200',
+    marginBottom:'20px',
+  },
+
+
 });
+
+
